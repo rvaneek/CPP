0382d9eb23dcfa92ff5df2e60573b65d14f6ad03
rvaneek <dubeyamar660@gmail.com>
2024-07-20 18:39:05 +0530

    Minor changes

--- Graph/Graph_1.cpp
+++ Graph/Graph_1.cpp
@@ -1,16 +1,14 @@
-#include <bits/stdc++.h>
 #include "Graph_1.h"
-using namespace std;
-
 
 int main() {
-    auto *g = new Graph();
+    const auto *g = new Graph();
     g->addEdge(1, 2);
     g->addEdge(4, 3);
     g->addEdge(3, 0);
 
-    cout<<g->hasEdge(4,2)<<endl;
-    cout<<g->hasEdge(4,3)<<endl;
+    cout << (g->hasEdge(4, 2) ? "There is an Edge" : "There is no Edge") << endl;
+    cout << (g->hasEdge(4, 3) ? "There is an Edge" : "There is no Edge") << endl;
+
 
     g->print();
     delete g;
